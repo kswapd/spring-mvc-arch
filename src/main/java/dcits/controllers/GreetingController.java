04296d680ddc77330b357ff223d7d561020ec5f9
kongxiangwen <xw.kong@hnair.com>
2018-05-21 15:06:14 +0800

    add initial front-end & backend rending module

---
+++ src/main/java/dcits/controllers/GreetingController.java
@@ -0,0 +1,35 @@
+package dcits.controllers;
+
+/**
+ * Created by kongxiangwen on 5/21/18 w:21.
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GreetingController {
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
+
+	@GetMapping("/greeting2")
+	public String greeting2(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting2";
+	}
+
+	@RequestMapping("/sb")
+	@ResponseBody
+	String home() {
+		return "Hello Spring boot!";
+	}
+
+}
