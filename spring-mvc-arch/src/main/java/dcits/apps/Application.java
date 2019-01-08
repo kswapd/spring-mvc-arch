edef7df5d2e3a2d22a34fa9abc94d39b574f4b56
kongxiangwen <xw.kong@hnair.com>
2019-01-08 16:05:55 +0800

    Add 3 types to generate a bean.

--- spring-mvc-arch/src/main/java/dcits/apps/Application.java
+++ spring-mvc-arch/src/main/java/dcits/apps/Application.java
@@ -3,7 +3,10 @@ package dcits.apps;
 /**
  * Created by kongxiangwen on 5/21/18 w:21.
  */
+import dcits.controllers.GreetingController;
 import dcits.models.People;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,18 +22,17 @@ import org.springframework.context.annotation.ImportResource;
 @ImportResource("classpath:new-schemas.xml")
 @ComponentScan("dcits")
 public class Application implements ApplicationContextAware {
-
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 	public static ApplicationContext context;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-
-
-
 	}
 
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-			context = applicationContext;
-		People p = context.getBean(People.class);
-		System.out.println("---------"+p.getAge());
+		context = applicationContext;
+		People p1 = (People)context.getBean("schemaPeople");
+		People p2 = (People)context.getBean("beanPeople");
+		People p3 = (People)context.getBean("configPeople");
+		logger.info("---------{},{},{}", p1.getAge(),p2.getAge(),p3.getAge());
 	}
 }
---
+++ spring-mvc-arch/src/main/java/dcits/configs/BeanConfig.java
@@ -0,0 +1,21 @@
+package dcits.configs;
+
+
+import dcits.models.People;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource(locations={"classpath:config-beans.xml"})
+public class BeanConfig {
+
+
+	@Bean
+	public People configPeople(){
+		People p = new People();
+		p.setAge(34);
+		return p;
+	}
+
+}
